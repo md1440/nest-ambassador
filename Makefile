0d23b5e8c8b98a0cda946894669e56ec64fe1c8f
Marc <91346941+md1440@users.noreply.github.com>
2022-03-26 22:58:54 +0100

    add users module, controller, service, entity, create db table

---
+++ Makefile
@@ -0,0 +1,10 @@
+# call with make
+
+build:
+  docker-compose build
+ 
+run:
+  docker-compose up -d
+ 
+stop:
+  docker-compose down
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { AppService } from './app.service';
       autoLoadEntities: true, // only for dev
       synchronize: true,
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
