764a5d648bafca570244d51029a2aeb6fc7b0092
Marc <91346941+md1440@users.noreply.github.com>
2022-03-27 17:40:57 +0200

    add register.dto

---
+++ src/auth/dtos/register.dto.ts
@@ -0,0 +1,23 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class RegisterDto {
+  @IsNotEmpty()
+  @IsString()
+  first_name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  last_name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password_confirm: string;
+}
