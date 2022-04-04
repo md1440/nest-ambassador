9831daa3c40b1a32a5554e6ee3f16ec58d198497
Marc <91346941+md1440@users.noreply.github.com>
2022-04-04 20:14:47 +0200

    implement product.controller functionality

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ProductCreateDto } from './dtos/product-create.dto';
 import { ProductService } from './product.service';
 
 @Controller()
@@ -8,5 +17,30 @@ export class ProductController {
   @Get('admin/products')
   async getAll() {
     return this.productService.find({});
+  }
+
+  @Post('admin/products')
+  async create(@Body() body: ProductCreateDto) {
+    return this.productService.save(body);
+  }
+
+  @Get('admin/products/:id')
+  async get(@Param('id') id: number) {
+    return this.productService.findOne({ id });
+  }
+
+  @Patch('admin/products/:id')
+  async update(
+    @Param('id') id: number,
+    @Body() body: Partial<ProductCreateDto>,
+  ) {
+    await this.productService.update(id, body);
+
+    return this.productService.find({ id });
+  }
+
+  @Delete('admin/products/:id')
+  async delete(@Param('id') id: number) {
+    return this.productService.delete(id);
   }
 }
