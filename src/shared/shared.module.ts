6193c25095a3a4932b4a7becff8d05703d2ac2ac
Marc <mdafeldecker@gmail.com>
2022-04-17 14:21:51 +0200

    add redis to docker, add cache-manager and redis to package.json and shared.module

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,11 +1,20 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import * as redisStore from 'cache-manager-redis-store';
+import { RedisClientOptions } from 'redis';
 
 @Module({
   imports: [
     JwtModule.register({
       secret: 'secret', // TODO: store in ENV
       signOptions: { expiresIn: '1d' },
+    }),
+    CacheModule.register<RedisClientOptions>({
+      store: redisStore,
+      socket: {
+        host: 'redis',
+        port: 6379,
+      },
     }),
   ],
   exports: [JwtModule],
